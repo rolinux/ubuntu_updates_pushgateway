b41b93488ef43c7c6d8b42ad647f1b416a0198e8
Radu Coroi <radu@mbpr.local>
2020-09-30 22:45:03 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ubuntu_updates_pushgateway
-Ansible playbook installing a shell script that will run on Ubuntu and send updates numbers to Pushgateway
+Ansible playbook for installing the `ubuntu-updates-to-pushgateway.sh` shell script that will run only on Ubuntu instances and send apt updates numbers to Pushgateway.
+
+Use the following Prometheus Alert code:
+
+```yaml
+- name: Software alerts
+	rules:
+	- alert: Ubuntu security updates
+	expr: ubuntu_updates_security{job="ubuntu_updates"} > 0
+	for: 5m
+	labels:
+		severity: warning
+	annotations:
+		title: Ubuntu security updates for {{ $labels.instance }} instance
+		description: There are {{ $value }} security updates for {{ $labels.instance }} instance
+```
---
+++ roles/ubuntu_updates_pushgateway/files/ubuntu-updates-to-pushgateway.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# comment enable echo/debug
+echo() { :; }
+
+# change it to your target Pushgateway
+PUSHGATEWAY="http://nuc6:30091"
+
+INSTANCE=`uname -n`
+
+echo $INSTANCE
+
+# parse updates total and security
+read -r TOTAL SECURITY <<< $(/usr/lib/update-notifier/apt-check 2>&1 | tr ';' ' ')
+
+echo $TOTAL
+echo $SECURITY
+
+# if zero updates then delete any existing gauge
+
+if (( $TOTAL == 0 )); then
+  echo "No upgrades"
+  /usr/bin/curl -X DELETE $PUSHGATEWAY/metrics/job/ubuntu_updates/instance/$INSTANCE
+  exit 0
+fi
+
+# if not zero set gauge
+echo "We have $TOTAL total upgrades and $SECURITY security upgrades"
+
+cat <<EOF | /usr/bin/curl --data-binary @- $PUSHGATEWAY/metrics/job/ubuntu_updates/instance/$INSTANCE
+# TYPE ubuntu_updates_total gauge
+ubuntu_updates_total{instance="$INSTANCE",job="ubuntu_updates"} $TOTAL
+# TYPE ubuntu_updates_security gauge
+ubuntu_updates_security{instance="$INSTANCE",job="ubuntu_updates"} $SECURITY
+EOF
